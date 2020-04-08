06a42ffed85c69762eee446542aba7498f58f084
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-04-08 19:03:37 +0800

    Added logic

--- ai/Layer.cpp
+++ ai/Layer.cpp
@@ -1,1 +1,17 @@
 #incude "Layer.hpp"
+
+Layer::Layout(std::vector<float> weights) : weights(weights), neuronQuantity(weights.size())
+{
+
+}
+
+/**
+ * Вычисление значения слоя
+ **/
+std::vector<float> Layer::calculate(std::vector<float> coefficients)
+{
+    std::vector<float> result;
+    math::Vector<float>::mul(this->weights, coefficients, result);
+
+    return result;
+}
